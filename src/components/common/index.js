c2d2e15f33ab5eb9a723e9588782ab8b32bc6ae1
webdevlicious <136869502+webdevlicious@users.noreply.github.com>
2025-01-31 16:28:08 -0500

    Add common components index

---
+++ src/components/common/index.js
@@ -0,0 +1,13 @@
+// Buttons
+export { default as IconButton } from './buttons/IconButton';
+export { default as PrimaryButton } from './buttons/PrimaryButton';
+
+// Layout
+export { default as Section } from './layout/Section';
+export { default as SectionHeading } from './layout/SectionHeading';
+
+// UI Components
+export { default as Card } from './ui/Card';
+export { default as GradientBackground } from './ui/GradientBackground';
+export { default as LoadingSpinner } from './ui/LoadingSpinner';
+export { default as Modal } from './ui/Modal';
